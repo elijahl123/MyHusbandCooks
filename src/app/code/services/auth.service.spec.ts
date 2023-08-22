b2ebc00b3f9b7e0fac90efdf177599abfd4b938c
Elijah Lopez <Elijah.kane.1972@gmail.com>
2023-08-22 14:28:33 -0700

    Fire Auth 9

--- src/app/code/services/auth.service.spec.ts
+++ src/app/code/services/auth.service.spec.ts
@@ -13,19 +13,10 @@ describe('AuthService', () => {
     expect(service).toBeTruthy();
   });
 
-  // Test for signIn method
-  it('should sign in a user', async () => {
-    const email = 'elijah.kane.1972@gmail.com'; // Replace with a valid test email
-    const password = 'elijahl123'; // Replace with a valid test password
-    const result = await service.signIn(email, password);
-    expect(result).toBeDefined();
-    // Additional assertions based on your authentication setup
-  });
-
   // Test for signUp method
   it('should sign up a user', async () => {
-    const email = 'test@example.com'; // Replace with a valid test email
-    const password = 'test-password'; // Replace with a valid test password
+    const email = 'test@example.com'; // Replace with a valid email
+    const password = 'test-password'; // Replace with a valid password
     const firstName = 'Test';
     const lastName = 'User';
     await service.signUp(email, password, firstName, lastName);
@@ -38,10 +29,26 @@ describe('AuthService', () => {
     // Additional assertions based on your authentication setup
   });
 
+  // Test for signIn method
+  it('should sign in a user', async () => {
+    const email = 'test@example.com'; // Replace with a valid email
+    const password = 'test-password'; // Replace with a valid password
+    const result = await service.signIn(email, password);
+    expect(result).toBeDefined();
+    // Additional assertions based on your authentication setup
+  });
+
   // Test for getUser method
   it('should get the currently signed-in user', async () => {
     const user = await service.getUser();
     expect(user).toBeDefined();
+    // Additional assertions based on your authentication setup
+    console.log(user);
+  });
+
+  // Test for deleteUser method
+  it('should delete a user', async () => {
+    await service.deleteUser();
     // Additional assertions based on your authentication setup
   });
 });
