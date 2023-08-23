006cdd1949a10bfd3f6e93541bda0d62533533ec
Elijah Lopez <Elijah.kane.1972@gmail.com>
2023-08-23 13:45:47 -0700

    Fire Post

--- src/app/code/components/comment/comment.component.ts
+++ src/app/code/components/comment/comment.component.ts
@@ -2,8 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Comment } from '../../interfaces/comment.model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PostService } from '../../services/post.service';
+import { getAuth } from 'firebase/auth';
+import { app } from '../../../app.module';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-comment',
@@ -11,11 +12,11 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
-  @Input() postId: string;
+  @Input() postId!: string;
   comments: Comment[] = [];
   commentForm: FormGroup;
 
-  constructor(private postService: PostService, private fb: FormBuilder, private router: Router, private authService: AuthService) {
+  constructor(private postService: PostService, private fb: FormBuilder, private router: Router) {
     this.commentForm = this.fb.group({
       content: ['', Validators.required]
     });
@@ -28,7 +29,7 @@ export class CommentComponent implements OnInit {
   }
 
   async addComment() {
-    const user = await this.authService.getUser();
+    const user = getAuth(app).currentUser;
     if (user) {
       const comment: Comment = this.commentForm.value;
       comment.authorId = user.uid; // Set the author ID from the current user
